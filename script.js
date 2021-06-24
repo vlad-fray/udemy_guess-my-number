6e37e2d552c8435e23fa0e196ea1509d8da94f91
VFray <rekrut313@gmail.com>
2021-06-24 12:57:19 +0300

    difficulty lvls

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 'use strict';
 
-const rangeNum = 20;
-const number = Math.trunc(Math.random() * rangeNum) + 1;
-let score = 20;
-document.querySelector('.range').textContent = `1 - ${rangeNum}`;
+let rangeNum = null;
+let number = null;
+let score = null;
+let highscore = 0;
+let difficulty = 1;
+
+document.querySelector('.difficulty').addEventListener('click', function () {
+	if (difficulty < 3) difficulty++;
+	document.querySelector('.difficulty-lvl').textContent = `Lvl: ${difficulty}`;
+	startGame();
+});
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = +document.querySelector('.guess').value;
-	document.querySelector('.message').textContent = checkGuess(guess);
+	displayMessage(checkGuess(guess));
 	if (score >= 1 && guess === number) {
-		if (document.querySelector('.highscore').textContent < score)
-			document.querySelector('.highscore').textContent = score;
-		document.querySelector('.number').textContent = number;
-		document.querySelector('body').style.backgroundColor = '#60b347';
-		document.querySelector('.number').style.width = '30rem';
-	} else if (score > 1) {
-		changeScore(score - 1);
+		winGame();
+	} else if (score > difficulty) {
+		changeScore(score - difficulty);
 	} else {
-		changeScore(0);
-		document.querySelector('.message').textContent = 'You lost a game!';
+		loseGame();
 	}
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-	document.querySelector('.message').textContent = 'Start guessing...';
-	document.querySelector('body').style.backgroundColor = '#222';
-	document.querySelector('.number').style.width = '15rem';
-	document.querySelector('.number').textContent = '?';
-	document.querySelector('.guess').value = '';
-	changeScore(20);
+	startGame();
 });
 
 const checkGuess = (guess) => {
@@ -49,3 +46,55 @@ const changeScore = (num) => {
 	score = num;
 	document.querySelector('.score').textContent = score;
 };
+
+const changeHighscore = (num) => {
+	if (document.querySelector('.highscore').textContent < num) {
+		document.querySelector('.highscore').textContent = num;
+		highscore = num;
+	}
+};
+
+const changeNumber = (num, width) => {
+	document.querySelector('.number').style.width = width;
+	document.querySelector('.number').textContent = num;
+};
+
+const toggleCheckAccess = (isDisabled) => {
+	document.querySelector('.check').disabled = isDisabled;
+};
+
+const displayMessage = (message) => {
+	document.querySelector('.message').textContent = message;
+};
+
+const changeRange = (num) => {
+	rangeNum = num;
+	document.querySelector('.range').textContent = `1 - ${rangeNum}`;
+	number = Math.trunc(Math.random() * rangeNum) + 1;
+};
+
+const winGame = () => {
+	changeHighscore(score);
+	changeNumber(number, '30rem');
+	toggleCheckAccess(true);
+	document.querySelector('body').style.backgroundColor = '#60b347';
+};
+
+const loseGame = () => {
+	changeScore(0);
+	toggleCheckAccess(true);
+	displayMessage('You lost a game!');
+};
+
+const startGame = () => {
+	changeRange(20 + 10 * (difficulty - 1));
+	score = 20;
+	displayMessage('Start guessing...');
+	document.querySelector('body').style.backgroundColor = '#222';
+	changeNumber('?', '15rem');
+	document.querySelector('.guess').value = '';
+	changeScore(20);
+	toggleCheckAccess(false);
+};
+
+startGame();
